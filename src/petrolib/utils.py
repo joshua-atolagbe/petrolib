b04f2ec9e3ac8fed37019704465bdda82d871f23
mayor-of-geology <atolagbejoshua2@gmail.com>
2023-05-04 05:27:44 +0100

    fix mnemonic error issue

--- src/petrolib/utils.py
+++ src/petrolib/utils.py
@@ -2,7 +2,6 @@ class MnemonicError(Exception):
     '''
     Exception raised if the log curve/menemonic passed is not found
     '''
-    def __init__(self, curve, msg):
-        self.curve = curve
+    def __init__(self, msg):
         self.msg = msg
         super().__init__(self.msg)
